def0cb6e0262464afedeb9f8f138a6bb6effe5e8
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 21:17:13 +0600

    problem-2 added

---
+++ problem-2.js
@@ -0,0 +1,13 @@
+/* ৩ এর নামতা লিখো জাভাস্ক্রিপ্ট ব্যাবহার করে। */
+
+// let num1 = 3;
+// let num2 = 5;
+// let product = num1 * num2;
+// console.log(product);
+
+/* --------------------------------- */
+
+for( let i = 1; i <= 10; i++){
+    // console.log(i);
+    console.log( i + '*' + 3 + ' = ' + i * 3)
+}
